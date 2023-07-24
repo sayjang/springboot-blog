25297311caf880a953b8351df644ca95b6dfabfa
jangsehee <tpgml2379@jiran.com>
2023-07-24 23:29:41 +0900

    spring security

--- src/main/java/me/sayjang/springbootblog/domain/Article.java
+++ src/main/java/me/sayjang/springbootblog/domain/Article.java
@@ -5,12 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)

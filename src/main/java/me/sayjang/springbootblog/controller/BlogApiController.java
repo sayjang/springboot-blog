37b9be10414d38471ea7790485013a43c2544383
jangsehee <tpgml2379@jiran.com>
2023-08-06 20:46:31 +0900

    validation add

--- src/main/java/me/sayjang/springbootblog/controller/BlogApiController.java
+++ src/main/java/me/sayjang/springbootblog/controller/BlogApiController.java
@@ -9,6 +9,7 @@ import me.sayjang.springbootblog.dto.UpdateArticleRequest;
 import me.sayjang.springbootblog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -28,7 +29,7 @@ public class BlogApiController {
      */
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request, Principal principal){
+    public ResponseEntity<Article> addArticle(@RequestBody @Validated AddArticleRequest request, Principal principal){
         Article savedArticle = blogService.save(request, principal.getName());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
--- src/main/java/me/sayjang/springbootblog/dto/AddArticleRequest.java
+++ src/main/java/me/sayjang/springbootblog/dto/AddArticleRequest.java
@@ -4,14 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.sayjang.springbootblog.domain.Article;
+import jakarta.validation.constraints.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class AddArticleRequest {
 
+    @NotNull
+    @Size(min = 1, max = 10)
     private String title;
+
+    @NotNull
     private String content;
+
     private String author;
 
     public Article toEntity(String author) {
